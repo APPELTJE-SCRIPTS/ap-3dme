25e73262abf872c2a4d41877278aa60b13cf8978
Sanity's Scripts <93653477+SN-SCRIPTS@users.noreply.github.com>
2022-02-25 11:05:11 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,5 +1,10 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
+description 'sn-3dme - 3dme script'
+
+author 'NICK#5496'
+
+
 client_script 'client.lua'
 server_script '@mysql-async/lib/MySQL.lua'
 server_script 'server.lua'
